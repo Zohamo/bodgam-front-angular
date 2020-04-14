dd1a95465acec51b5c3c573a89b288b8cfabb691
Mathias Mille <mathias.mille@altran.com>
2020-04-14 15:28:32 +0200

    :bug: open LOCATION detail from event

--- src/app/content/event/shared/components/event-location/event-location.component.ts
+++ src/app/content/event/shared/components/event-location/event-location.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { LocationItem } from '@/models';
 import { faBuilding } from '@fortawesome/free-solid-svg-icons';
+
+// Entry Components
+import { LocationDetailDialogComponent } from '@/content/location/components';
 
 @Component({
   selector: 'app-event-location',
@@ -11,4 +15,25 @@ export class EventLocationComponent {
   faBuilding = faBuilding;
   @Input() location: LocationItem;
   @Input() showLocation: boolean;
+
+  /**
+   * Creates an instance of EventLocationComponent.
+   *
+   * @param {MatDialog} dialog
+   * @memberof EventLocationComponent
+   */
+  constructor(private dialog: MatDialog) {}
+
+  /**
+   * OnEvent open the Location detail dialog.
+   *
+   * @memberof EventLocationComponent
+   */
+  public onOpenLocationDetailDialog(): void {
+    if (this.showLocation) {
+      this.dialog.open(LocationDetailDialogComponent, {
+        data: { id: this.location.id }
+      });
+    }
+  }
 }
