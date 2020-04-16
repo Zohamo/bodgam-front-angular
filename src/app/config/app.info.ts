8c77608d3034dc063f03032f13a7b0c2fb7d0a7e
Mathias Mille <mathias.mille@altran.com>
2020-04-16 12:04:55 +0200

    :green_book: edit version + changelog

--- src/app/config/app.info.ts
+++ src/app/config/app.info.ts
@@ -1,6 +1,6 @@
 export const AppInfo = {
   TITLE: 'BodGam',
-  VERSION: '0.1.0.1',
+  VERSION: '0.1.0.2',
   AUTHOR: { name: 'Mathias Mille', url: 'https://mathiasmille.fr' },
   STARTYEAR: '2018'
 };
